c0c0e5eee9ce17ff08cac7cc61b6d9809673d6c9
IneshV <123998765+IneshV@users.noreply.github.com>
2023-11-15 14:01:52 -0500

    Create Voice_generator.py

---
+++ Voice_generator.py
@@ -0,0 +1,12 @@
+# Import PyTorch or TensorFlow 
+
+def train_voice_cloning_model(folder_of_voice_samples):
+    # Implement voice cloning training logic using the deep learning model
+    # Save the trained model for future use
+  return 
+
+  
+
+def generate_audio_from_pdf(text_from_pdf_file, model_folder_path):
+    # Implement PDF to audio conversion logic using the deep learning model
+    # Return the generated audio
